936dad307eaba74275763261bd469d1923bc82ce
Jaap Blom <jblom@beeldengeluid.nl>
2015-03-10 11:08:50 +0100

    hopefully a fix on the server

--- src/static/js/select.js
+++ src/static/js/select.js
@@ -1,17 +1,17 @@
 var _input = {
  "ID": "123124asfwr234",
  "description": "I want to find information on the Dutch royal family.",
- "relevant": [ // items marked as relevant
+ "relevant": [
     {
       "videoURL": "http://axes.ch.bbc.co.uk/collections/cAXES/videos/cAXES/v20080516_100000_bbcone_to_buy_or_not_to_buy.webm",
       "keyframeURL": "",
-      "start": 12123, // time in milli seconds
+      "start": 12123,
       "end": 12314,
     },
     {
       "videoURL": "http://axes.ch.bbc.co.uk/collections/cAXES/videos/cAXES/v20080621_200000_bbctwo_andrew_marrs_history_of.webm",
       "keyframeURL": "",
-      "start": 12123, // time in milli seconds
+      "start": 12123,
       "end": 12314,
     }
  ]
